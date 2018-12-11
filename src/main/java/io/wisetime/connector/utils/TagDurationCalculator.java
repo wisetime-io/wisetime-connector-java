b1f18a4618b8430107fc024c711ddfbeaa3b1cb8
Alvin Llobrera <alvin.llobrera@practiceinsight.io>
2018-12-11 20:28:18 +0800

    WT-6153 Enable calculation of tag duration based on row's total duration
    - Patricia needs the actual worked time when recording Time Registration

--- src/main/java/io/wisetime/connector/utils/TagDurationCalculator.java
+++ src/main/java/io/wisetime/connector/utils/TagDurationCalculator.java
@@ -5,6 +5,7 @@
 package io.wisetime.connector.utils;
 
 import io.wisetime.generated.connect.TimeGroup;
+import io.wisetime.generated.connect.TimeRow;
 
 /**
  * @author shane.xie@practiceinsight.io
@@ -19,7 +20,7 @@ public class TagDurationCalculator {
    * @return duration, in seconds, to apply to each tag
    */
   public static double tagDuration(final TimeGroup timeGroup) {
-    return tagDuration(timeGroup, false);
+    return tagDuration(timeGroup, true, false);
   }
 
   /**
@@ -30,18 +31,33 @@ public class TagDurationCalculator {
    * @return duration, in seconds, to apply to each tag
    */
   public static double tagDurationDisregardingExperienceRating(final TimeGroup timeGroup) {
-    return tagDuration(timeGroup, true);
+    return tagDuration(timeGroup, true, true);
   }
 
-  private static double tagDuration(final TimeGroup timeGroup, final boolean disregardExperienceRating) {
+
+  /**
+   * Calculate the tag duration to apply to each tag.
+   *
+   * @param timeGroup the TimeGroup for which to calculate each tag duration
+   * @param useTimeGroupDuration if True, the method will use the time group's total duration to calculate tag duration,
+   *                             otherwise, it will use the sum of the duration of each time row in the time group.
+   * @param disregardExperienceRating if True, it will apply the user experience rating to calculate tag duration, otherwise,
+   *                                  it will disregard user's experience rating.
+   * @return duration, in seconds, to apply to each tag
+   */
+  public static double tagDuration(final TimeGroup timeGroup,
+                                   final boolean useTimeGroupDuration,
+                                   final boolean disregardExperienceRating) {
     if (timeGroup.getTags().size() == 0) {
       return 0;
     }
 
-    double totalDuration = timeGroup.getTotalDurationSecs();
+    double totalDuration = useTimeGroupDuration
+        ? timeGroup.getTotalDurationSecs()
+        : timeGroup.getTimeRows().stream().mapToDouble(TimeRow::getDurationSecs).sum();
 
     if (!disregardExperienceRating) {
-      totalDuration = timeGroup.getTotalDurationSecs() * timeGroup.getUser().getExperienceWeightingPercent() / 100.0;
+      totalDuration = totalDuration * timeGroup.getUser().getExperienceWeightingPercent() / 100.0;
     }
 
     switch (timeGroup.getDurationSplitStrategy()) {
--- src/test/java/io/wisetime/connector/utils/TagDurationCalculatorTest.java
+++ src/test/java/io/wisetime/connector/utils/TagDurationCalculatorTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import io.wisetime.connector.testutils.FakeEntities;
 import io.wisetime.generated.connect.TimeGroup;
+import io.wisetime.generated.connect.TimeRow;
 import io.wisetime.generated.connect.User;
 
 import static io.wisetime.connector.utils.TagDurationCalculator.*;
@@ -27,8 +28,8 @@ class TagDurationCalculatorTest {
     final TimeGroup timeGroupWithNoTags = fakeEntities.randomTimeGroup().tags(ImmutableList.of());
 
     assertThat(tagDuration(timeGroupWithNoTags))
-        .isEqualTo(0)
-        .as("Tag duration should be zero if there are no tags");
+        .as("Tag duration should be zero if there are no tags")
+        .isEqualTo(0);
   }
 
   @Test
@@ -37,8 +38,8 @@ class TagDurationCalculatorTest {
     final TimeGroup timeGroup = fakeEntities.randomTimeGroup().user(userWithNoExperience);
 
     assertThat(tagDuration(timeGroup))
-        .isEqualTo(0)
-        .as("Zero experience rating should result in zero duration to each tag");
+        .as("Zero experience rating should result in zero duration to each tag")
+        .isEqualTo(0);
   }
 
   @Test
@@ -50,9 +51,9 @@ class TagDurationCalculatorTest {
         .durationSplitStrategy(TimeGroup.DurationSplitStrategyEnum.DIVIDE_BETWEEN_TAGS);
 
     assertThat(tagDuration(timeGroup))
-        .isEqualTo(10.5 / timeGroup.getTags().size())
         .as("Calculated duration should take into account experience rating and" +
-            "split the total duration between the tags");
+            "split the total duration between the tags")
+        .isEqualTo(10.5 / timeGroup.getTags().size());
   }
 
   @Test
@@ -64,9 +65,9 @@ class TagDurationCalculatorTest {
         .durationSplitStrategy(TimeGroup.DurationSplitStrategyEnum.WHOLE_DURATION_TO_EACH_TAG);
 
     assertThat(tagDuration(timeGroup))
-        .isEqualTo(10)
         .as("Calculated duration should take into account experience rating and " +
-            "assign whole total duration to each tag");
+            "assign whole total duration to each tag")
+        .isEqualTo(10);
   }
 
   @Test
@@ -78,7 +79,34 @@ class TagDurationCalculatorTest {
         .durationSplitStrategy(TimeGroup.DurationSplitStrategyEnum.WHOLE_DURATION_TO_EACH_TAG);
 
     assertThat(tagDurationDisregardingExperienceRating(timeGroup))
-        .isEqualTo(100)
-        .as("Calculated duration should not take into account experience rating");
+        .as("Calculated duration should not take into account experience rating")
+        .isEqualTo(100);
+  }
+
+  @Test
+  void tagDuration_should_use_time_group_duration() {
+    final User user = fakeEntities.randomUser().experienceWeightingPercent(10);
+    final TimeGroup timeGroup = fakeEntities.randomTimeGroup()
+        .user(user)
+        .totalDurationSecs(100)
+        .durationSplitStrategy(TimeGroup.DurationSplitStrategyEnum.WHOLE_DURATION_TO_EACH_TAG);
+
+    assertThat(tagDuration(timeGroup, true, true))
+        .as("Calculated duration should use the defined time group duration")
+        .isEqualTo(100);
+  }
+
+  @Test
+  void tagDuration_should_use_time_rows_total_duration() {
+    final User user = fakeEntities.randomUser().experienceWeightingPercent(10);
+    final TimeGroup timeGroup = fakeEntities.randomTimeGroup()
+        .user(user)
+        .totalDurationSecs(100)
+        .durationSplitStrategy(TimeGroup.DurationSplitStrategyEnum.WHOLE_DURATION_TO_EACH_TAG);
+    timeGroup.getTimeRows().get(0).setDurationSecs(200);
+
+    assertThat(tagDuration(timeGroup, false, true))
+        .as("Calculated duration should use the actual total duration of all time rows in the group")
+        .isEqualTo(timeGroup.getTimeRows().stream().mapToDouble(TimeRow::getDurationSecs).sum());
   }
 }
