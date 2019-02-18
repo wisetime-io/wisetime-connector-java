4cd1663391cec20566ebc29c02460ba9adc52b08
Alvin Llobrera <alvin.llobrera@practiceinsight.io>
2019-02-18 14:34:33 +0800

    Added assertion notes on ActivityTimeCalculatorTest

--- src/test/java/io/wisetime/connector/utils/ActivityTimeCalculatorTest.java
+++ src/test/java/io/wisetime/connector/utils/ActivityTimeCalculatorTest.java
@@ -40,33 +40,32 @@ class ActivityTimeCalculatorTest {
     final TimeRow row2 = fakeEntities.randomTimeRow().activityHour(2018110209).firstObservedInHour(29);
     final TimeGroup timeGroup = fakeEntities.randomTimeGroup().timeRows(Lists.newArrayList(row1, row2));
 
-    // by minute
     assertThat(startTime(timeGroup))
-        .as("The start time is the segment hour of the earliest time row")
+        .as("should be the activity time of the earliest time row from the time group")
         .contains(LocalDateTime.of(2018, 11, 2, 9, 3));
 
-    // by hour
+    // add time row with earliest activity time by hour
     timeGroup.getTimeRows().add(fakeEntities.randomTimeRow().activityHour(2018110208).firstObservedInHour(29));
     assertThat(startTime(timeGroup))
-        .as("The start time is the segment hour of the earliest time row")
+        .as("should be able to check the earliest time row by hour")
         .contains(LocalDateTime.of(2018, 11, 2, 8, 29));
 
-    // by dayOfMonth
+    // add time row with earliest activity time by day of month
     timeGroup.getTimeRows().add(fakeEntities.randomTimeRow().activityHour(2018110108).firstObservedInHour(29));
     assertThat(startTime(timeGroup))
-        .as("The start time is the segment hour of the earliest time row")
+        .as("should be able to check the earliest time row by day of month")
         .contains(LocalDateTime.of(2018, 11, 1, 8, 29));
 
-    // by month
+    // add time row with earliest activity time by month
     timeGroup.getTimeRows().add(fakeEntities.randomTimeRow().activityHour(2018100108).firstObservedInHour(29));
     assertThat(startTime(timeGroup))
-        .as("The start time is the segment hour of the earliest time row")
+        .as("should be able to check the earliest time row by month")
         .contains(LocalDateTime.of(2018, 10, 1, 8, 29));
 
-    // by year
+    // add time row with earliest activity time by year
     timeGroup.getTimeRows().add(fakeEntities.randomTimeRow().activityHour(1990100108).firstObservedInHour(29));
     assertThat(startTime(timeGroup))
-        .as("The start time is the segment hour of the earliest time row")
+        .as("should be able to check the earliest time row by year")
         .contains(LocalDateTime.of(1990, 10, 1, 8, 29));
   }
 }
