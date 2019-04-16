4b241276a690ae585a91583a5e110ce9ff3176e3
thomas <tom.haines@staff.wisetime.com>
2019-04-16 23:40:10 +0800

    Add cascade CI build step

---
+++ bin/cascade.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -o errexit ; set -o errtrace ; set -o pipefail
+
+curl -q --user ${BAMBOO_USER}:${BAMBOO_PASS} \
+  -X POST -d "STAGE1&ExecuteAllStages" \
+  https://bamboo.dev.wisetime.com/rest/api/latest/queue/CAPI-WCUT
