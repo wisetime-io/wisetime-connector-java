373b36266c2e39bd1c6f43ae3d50a9b3c74b19d5
Yehor Lashkul <yegor.lashkul@gmail.com>
2019-04-25 15:44:26 +0300

    WT-7089 add RuntimeConfigPropertyPropertyDefiner test

---
+++ src/test/java/io/wisetime/connector/logging/RuntimeConfigPropertyDefinerTest.java
@@ -0,0 +1,42 @@
+package io.wisetime.connector.logging;
+
+import org.junit.jupiter.api.Test;
+
+import io.wisetime.connector.config.RuntimeConfig;
+import io.wisetime.connector.config.RuntimeConfigKey;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author yehor.lashkul
+ */
+class RuntimeConfigPropertyDefinerTest {
+
+  private final String propertyKey = "test-key";
+  private final RuntimeConfigKey configKey = () -> propertyKey;
+
+  @Test
+  void getPropertyValue_configExist() {
+    RuntimeConfig.setProperty(configKey, "test-value");
+    RuntimeConfigPropertyDefiner propertyDefiner = new RuntimeConfigPropertyDefiner();
+    propertyDefiner.setKey(propertyKey);
+    assertThat(propertyDefiner.getPropertyValue()).isEqualTo("test-value");
+  }
+
+  @Test
+  void getPropertyValue_noConfigDefaultSet() {
+    RuntimeConfig.clearProperty(configKey);
+    RuntimeConfigPropertyDefiner propertyDefiner = new RuntimeConfigPropertyDefiner();
+    propertyDefiner.setKey(propertyKey);
+    propertyDefiner.setDefaultValue("test-default-value");
+    assertThat(propertyDefiner.getPropertyValue()).isEqualTo("test-default-value");
+  }
+
+  @Test
+  void getPropertyValue_noConfigNoDefault() {
+    RuntimeConfig.clearProperty(configKey);
+    RuntimeConfigPropertyDefiner propertyDefiner = new RuntimeConfigPropertyDefiner();
+    propertyDefiner.setKey(propertyKey);
+    assertThat(propertyDefiner.getPropertyValue()).isEqualTo("");
+  }
+}
