93cf8302b632954b8cff47a8f8a64a342a2647e6
vadym <sukolenvo@gmail.com>
2019-05-21 20:28:36 +0300

    WT-7270 fix mirror tags

--- bin/mirror.sh
+++ bin/mirror.sh
@@ -2,6 +2,7 @@
 set -o errexit ; set -o errtrace ; set -o pipefail
 
 git fetch --tags
+git checkout master
 mkdir -p ~/.ssh
 # Ensure we can talk to GitHub
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
@@ -10,7 +11,6 @@ echo "${GITHUB_SSH_KEY_B64}" | base64 -d > /tmp/.ssh/github.key
 chmod 600 /tmp/.ssh/github.key
 # Push to GitHub mirror
 git remote add github git@github.com:wisetime-io/wisetime-connector-java.git
-GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push github master
-GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push github --tags
+GIT_SSH_COMMAND='ssh -i /tmp/.ssh/github.key' git push github master --tags
 rm -rf /tmp/.ssh
 echo "Push to GitHub mirror complete"
