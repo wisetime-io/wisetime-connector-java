879001d789cd06772b33ade2759a0fe96c9a0b2b
Yehor Lashkul <yegor.lashkul@gmail.com>
2019-05-22 21:21:51 +0300

    WT-7089 Fix logger initialization

--- src/main/java/io/wisetime/connector/config/RuntimeConfig.java
+++ src/main/java/io/wisetime/connector/config/RuntimeConfig.java
@@ -11,6 +11,8 @@ import org.apache.commons.configuration2.CompositeConfiguration;
 import org.apache.commons.configuration2.MapConfiguration;
 import org.apache.commons.configuration2.SystemConfiguration;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -30,9 +32,14 @@ import static io.wisetime.connector.config.ConnectorConfigKey.CONNECTOR_PROPERTI
 @SuppressWarnings("WeakerAccess")
 public class RuntimeConfig {
 
+  // Don't remove this logger!!!
+  // Because our logback configurations depends on RuntimeConfig instance, first logger creation
+  // shouldn't happen during RuntimeConfig construction. To be sure this doesn't happen we initialize logger here
+  // and use lazy RuntimeConfig instance creation.
+  @SuppressWarnings("unused")
+  private static final Logger log = LoggerFactory.getLogger(RuntimeConfig.class);
   private final CompositeConfiguration config;
   private final MapConfiguration inMemoryConfig;
-  private static RuntimeConfig INSTANCE = new RuntimeConfig();
 
   private RuntimeConfig() {
     config = new CompositeConfiguration();
@@ -88,7 +95,7 @@ public class RuntimeConfig {
    * @return the Integer value, or an empty optional if there is no value configured for the key
    */
   public static Optional<Integer> getInt(RuntimeConfigKey configKey) {
-    return INSTANCE.getInt(configKey.getConfigKey());
+    return getInstance().getInt(configKey.getConfigKey());
   }
 
   /**
@@ -98,7 +105,7 @@ public class RuntimeConfig {
    * @return the String value, or an empty optional if there is no value configured for the key
    */
   public static Optional<String> getString(RuntimeConfigKey configKey) {
-    return INSTANCE.getString(configKey.getConfigKey());
+    return getInstance().getString(configKey.getConfigKey());
   }
 
   /**
@@ -108,7 +115,7 @@ public class RuntimeConfig {
    * @return the Boolean value, or an empty optional if there is no value configured for the key
    */
   public static Optional<Boolean> getBoolean(RuntimeConfigKey configKey) {
-    return INSTANCE.getBoolean(configKey.getConfigKey());
+    return getInstance().getBoolean(configKey.getConfigKey());
   }
 
   /**
@@ -118,7 +125,7 @@ public class RuntimeConfig {
    * but not system properties.
    */
   public static void setProperty(RuntimeConfigKey configKey, String value) {
-    INSTANCE.inMemoryConfig.setProperty(configKey.getConfigKey(), value);
+    getInstance().inMemoryConfig.setProperty(configKey.getConfigKey(), value);
   }
 
   /**
@@ -126,12 +133,12 @@ public class RuntimeConfig {
    * This method will not effect properties provided from environment config or file config.
    */
   public static void clearProperty(RuntimeConfigKey configKey) {
-    INSTANCE.inMemoryConfig.clearProperty(configKey.getConfigKey());
+    getInstance().inMemoryConfig.clearProperty(configKey.getConfigKey());
   }
 
   @VisibleForTesting
   public static void rebuild() {
-    INSTANCE = new RuntimeConfig();
+    LazyInstanceHolder.INSTANCE = new RuntimeConfig();
   }
 
   static Map<String, String> addUserPropertyFile(String filePath) {
@@ -155,6 +162,15 @@ public class RuntimeConfig {
     return configMap;
   }
 
+  // Use lazy instance initialization to prevent screwing logger configuration up.
+  // More details see in the logger creation comment above
+  private static RuntimeConfig getInstance() {
+    return LazyInstanceHolder.INSTANCE;
+  }
+
+  private static class LazyInstanceHolder {
+    public static RuntimeConfig INSTANCE = new RuntimeConfig();
+  }
 
   static class CaseInsensitiveSystemConfiguration extends SystemConfiguration {
     @SuppressWarnings("unchecked")
