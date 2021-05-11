b980e6f40fe423c3a123777912e87bdf5caeb8d8
Thomas Haines <thomas.haines@gmail.com>
2021-05-11 14:18:19 +0800

    Add common interface pattern for adjunct keywords LB-84

---
+++ src/main/java/io/wisetime/connector/adjunct_keywords/KeywordExtractor.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.adjunct_keywords;
+
+import io.wisetime.connector.datastore.ConnectorStore;
+import java.util.function.Consumer;
+
+/**
+ * Used to support keyword augmenters such as document management file ids and the like.
+ *
+ * @author thomas.haines
+ */
+public interface KeywordExtractor {
+
+  void fetchKeywords(ConnectorStore connectorStore, Consumer<KeywordUpdate> keywordUpdate);
+
+}
---
+++ src/main/java/io/wisetime/connector/adjunct_keywords/KeywordUpdate.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.adjunct_keywords;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author thomas.haines
+ */
+@Data
+@Accessors(chain = true)
+class KeywordUpdate {
+
+  private TagId tagId = new TagId();
+  private List<String> additionalKeywords = new ArrayList<>();
+
+}
