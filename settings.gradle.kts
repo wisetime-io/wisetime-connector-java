68f46e5a00b9a5c6c2b02488cc270408c56d180a
Vadym <vadym.sukolen+automation@wisetime.com>
2022-06-27 14:52:28 +0800

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
   plugins {
     id("com.github.ben-manes.versions") version "0.39.0"
-    id("io.wisetime.versionChecker") version "10.12.25"
+    id("io.wisetime.versionChecker") version "10.12.26"
     id("io.freefair.lombok") version "6.4.1"
   }
   repositories {
