fac75478f67e74f72ddacf35412c4055cbe8e31a
gradle-update-robot <gradle-update-robot@regolo.cc>
2024-11-12 03:05:02 +0000

    Mirror Repo

---
+++ wisetime-connector-template-formatter/src/test/java/io/wisetime/connector/template/loader/FileTemplateLoaderHelperTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2017 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.template.loader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author vadym
+ */
+public class FileTemplateLoaderHelperTest {
+
+  private FileTemplateLoaderHelper fileTemplateLoaderHelper;
+  private File template;
+
+  @BeforeEach
+  void setup() throws Exception {
+    template = File.createTempFile("template", ".ftl");
+    fileTemplateLoaderHelper = new FileTemplateLoaderHelper(template.getCanonicalPath());
+  }
+
+  @AfterEach
+  void cleanup() {
+    template.delete();
+  }
+
+  @Test
+  void createTemplateLoader() throws Exception {
+    Object templateSource = fileTemplateLoaderHelper.createTemplateLoader()
+        .findTemplateSource(fileTemplateLoaderHelper.getTemplateName());
+
+    assertThat(templateSource.toString())
+        .contains(template.toString());
+  }
+
+  @Test
+  void getTemplateName() {
+    assertThat(fileTemplateLoaderHelper.getTemplateName())
+        .isEqualTo(template.getName());
+  }
+}
