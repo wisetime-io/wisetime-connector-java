a1bd392369e2888c8d9d384872bc36cff8d9eae8
Vadym Sukolen <vsukolen@anaqua.com>
2025-12-04 08:24:48 +0000

    Mirror Repo

---
+++ wisetime-connector/src/main/java/io/wisetime/connector/controller/ConnectorControllerImpl.java
@@ -0,0 +1,347 @@
+/*
+ * Copyright (c) 2019 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.controller;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+import io.wisetime.connector.ConnectorController;
+import io.wisetime.connector.ConnectorModule;
+import io.wisetime.connector.ConnectorModule.IntervalConfig;
+import io.wisetime.connector.WiseTimeConnector;
+import io.wisetime.connector.activity_type.ActivityTypeRunner;
+import io.wisetime.connector.activity_type.ActivityTypeSlowLoopRunner;
+import io.wisetime.connector.activity_type.NoOpActivityTypeRunner;
+import io.wisetime.connector.activity_type.NoOpActivityTypeSlowLoopRunner;
+import io.wisetime.connector.api_client.ApiClient;
+import io.wisetime.connector.config.ManagedConfigRunner;
+import io.wisetime.connector.config.info.ConnectorInfoProvider;
+import io.wisetime.connector.config.info.ConstantConnectorInfoProvider;
+import io.wisetime.connector.datastore.FileStore;
+import io.wisetime.connector.datastore.SqLiteHelper;
+import io.wisetime.connector.health.HealthCheck;
+import io.wisetime.connector.health.HealthIndicator;
+import io.wisetime.connector.metric.ApiClientMetricWrapper;
+import io.wisetime.connector.metric.MetricInfo;
+import io.wisetime.connector.metric.MetricService;
+import io.wisetime.connector.tag.ApiClientTagWrapper;
+import io.wisetime.connector.tag.NoOpTagRunner;
+import io.wisetime.connector.tag.NoOpTagSlowLoopRunner;
+import io.wisetime.connector.tag.TagRunner;
+import io.wisetime.connector.tag.TagSlowLoopRunner;
+import io.wisetime.connector.time_poster.NoOpTimePoster;
+import io.wisetime.connector.time_poster.TimePoster;
+import io.wisetime.connector.time_poster.long_polling.FetchClientTimePoster;
+import java.util.Timer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Implementation of {@link ConnectorController}
+ *
+ * @author thomas.haines
+ * @author pascal.filippi
+ * @author shane.xie
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class ConnectorControllerImpl implements ConnectorController, HealthIndicator {
+
+
+  private final AtomicReference<ExecutorService> connectorExecutor = new AtomicReference<>();
+  private final TimePoster timePoster;
+  private final WiseTimeConnector wiseTimeConnector;
+  private final ConnectorModule connectorModule;
+
+  private final TagRunner tagRunner;
+  private final TagSlowLoopRunner tagSlowLoopRunner;
+  private final ActivityTypeRunner activityTypeRunner;
+  private final ActivityTypeSlowLoopRunner activityTypeSlowLoopRunner;
+  private final HealthCheck healthRunner;
+  private final ManagedConfigRunner managedConfigRunner;
+
+  private final MetricService metricService;
+
+  private final Timer healthCheckTimer;
+  private final Timer tagTimer;
+  private final Timer tagSlowLoopTimer;
+  private final Timer activityTypeTimer;
+  private final Timer activityTypeSlowLoopTimer;
+  private final Timer managedConfigTimer;
+
+  @Getter
+  private final TimerTaskSchedule tagTaskSchedule;
+  @Getter
+  private final TimerTaskSchedule tagSlowLoopTaskSchedule;
+  @Getter
+  private final TimerTaskSchedule activityTypeTaskSchedule;
+  @Getter
+  private final TimerTaskSchedule activityTypeSlowLoopTaskSchedule;
+
+  @Getter
+  private final TimerTaskSchedule healthTaskSchedule;
+
+  ConnectorControllerImpl(ConnectorControllerConfiguration configuration) {
+    metricService = new MetricService();
+    wiseTimeConnector = configuration.getWiseTimeConnector();
+
+    tagTaskSchedule = new TimerTaskSchedule(
+        TimeUnit.SECONDS.toMillis(15),
+        TimeUnit.MINUTES.toMillis(configuration.getTagSyncIntervalMinutes()));
+    tagSlowLoopTaskSchedule = new TimerTaskSchedule(
+        TimeUnit.SECONDS.toMillis(15),
+        TimeUnit.MINUTES.toMillis(configuration.getTagSyncSlowLoopIntervalMinutes()));
+    activityTypeTaskSchedule = new TimerTaskSchedule(
+        TimeUnit.SECONDS.toMillis(15),
+        TimeUnit.MINUTES.toMillis(configuration.getActivityTypeSyncIntervalMinutes()));
+    activityTypeSlowLoopTaskSchedule = new TimerTaskSchedule(
+        TimeUnit.MINUTES.toMillis(1),
+        TimeUnit.MINUTES.toMillis(configuration.getActivityTypeSyncSlowLoopIntervalMinutes()));
+
+    tagRunner = createTagRunner(configuration, wiseTimeConnector);
+    tagSlowLoopRunner = createTagSlowLoopRunner(configuration, wiseTimeConnector);
+
+    activityTypeRunner = createActivityTypeRunner(configuration, wiseTimeConnector);
+    activityTypeSlowLoopRunner = createActivityTypeSlowLoopRunner(configuration, wiseTimeConnector);
+
+    ApiClient apiClient = new ApiClientMetricWrapper(configuration.getApiClient(), metricService);
+    apiClient = new ApiClientTagWrapper(apiClient, tagRunner);
+
+    healthTaskSchedule = new TimerTaskSchedule(
+        TimeUnit.SECONDS.toMillis(5),
+        TimeUnit.MINUTES.toMillis(1)
+    );
+    healthRunner = new HealthCheck(apiClient, wiseTimeConnector);
+
+    final SqLiteHelper sqLiteHelper = new SqLiteHelper(configuration.isForcePersistentStorage());
+    final FileStore fileStore = FileStore.getInstance(sqLiteHelper);
+
+    connectorModule = new ConnectorModule(
+        apiClient,
+        fileStore,
+        IntervalConfig.builder()
+            .setTagIntervalMinutes(
+                (int) MILLISECONDS.toMinutes(tagTaskSchedule.getPeriodMs()))
+            .setTagSlowLoopIntervalMinutes(
+                (int) MILLISECONDS.toMinutes(tagSlowLoopTaskSchedule.getPeriodMs()))
+            .setActivityTypeIntervalMinutes(
+                (int) MILLISECONDS.toMinutes(activityTypeTaskSchedule.getPeriodMs()))
+            .setActivityTypeSlowLoopIntervalMinutes(
+                (int) MILLISECONDS.toMinutes(activityTypeSlowLoopTaskSchedule.getPeriodMs()))
+            .build()
+    );
+
+    final ConnectorInfoProvider connectorInfoProvider = new ConstantConnectorInfoProvider();
+    timePoster = createTimePoster(configuration, apiClient, sqLiteHelper);
+
+    managedConfigRunner = new ManagedConfigRunner(wiseTimeConnector, apiClient, connectorInfoProvider);
+
+    healthRunner.addHealthIndicator(tagRunner,
+        tagSlowLoopRunner,
+        activityTypeRunner,
+        activityTypeSlowLoopRunner,
+        timePoster,
+        managedConfigRunner);
+
+    healthCheckTimer = new Timer("health-check-timer", true);
+    tagTimer = new Timer("tag-check-timer", true);
+    tagSlowLoopTimer = new Timer("tag-slow-loop-timer", true);
+    activityTypeTimer = new Timer("activity-type-timer", true);
+    activityTypeSlowLoopTimer = new Timer("activity-type-slow-loop-timer", true);
+    managedConfigTimer = new Timer("manage-config-timer", true);
+  }
+
+  /**
+   * <pre>
+   *  Start the runner.
+   *  This will run tasks connected to tag updates, posted time and health checks.
+   *  This method call is blocking, meaning that the current thread will wait until the application stops.
+   * </pre>
+   */
+  @Override
+  public void start() throws Exception {
+    if (!connectorExecutor.compareAndSet(null, Executors.newCachedThreadPool())) {
+      log.warn("Cannot start the connector while it is still running. Start attempt ignored.");
+      return;
+    }
+
+    // Init may take a while to complete if connector runs self checks
+    wiseTimeConnector.init(connectorModule);
+
+    try {
+      timePoster.start();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    healthRunner.setShutdownFunction(this::stop);
+
+    healthCheckTimer.scheduleAtFixedRate(healthRunner,
+        healthTaskSchedule.getInitialDelayMs(), healthTaskSchedule.getPeriodMs());
+
+    tagTimer.scheduleAtFixedRate(tagRunner,
+        tagTaskSchedule.getInitialDelayMs(), tagTaskSchedule.getPeriodMs());
+
+    tagSlowLoopTimer.scheduleAtFixedRate(tagSlowLoopRunner,
+        tagSlowLoopTaskSchedule.getInitialDelayMs(), tagSlowLoopTaskSchedule.getPeriodMs());
+
+    activityTypeTimer.scheduleAtFixedRate(activityTypeRunner,
+        activityTypeTaskSchedule.getInitialDelayMs(), activityTypeTaskSchedule.getPeriodMs());
+
+    activityTypeSlowLoopTimer.scheduleAtFixedRate(activityTypeSlowLoopRunner,
+        activityTypeSlowLoopTaskSchedule.getInitialDelayMs(), activityTypeSlowLoopTaskSchedule.getPeriodMs());
+
+    TimerTaskSchedule managedConfigTaskSchedule = new TimerTaskSchedule(
+        TimeUnit.SECONDS.toMillis(15),
+        TimeUnit.MINUTES.toMillis(5));
+    managedConfigTimer.scheduleAtFixedRate(managedConfigRunner,
+        managedConfigTaskSchedule.getInitialDelayMs(), managedConfigTaskSchedule.getPeriodMs());
+
+    final boolean terminateSuccess = connectorExecutor.get().awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+    log.info("Connector stopped, graceful termination: {}", terminateSuccess);
+  }
+
+  @Override
+  public void stop() {
+    log.info("Stopping connector");
+    try {
+      healthRunner.cancel();
+      healthCheckTimer.cancel();
+      healthCheckTimer.purge();
+
+      managedConfigTimer.cancel();
+      managedConfigTimer.purge();
+
+      tagTimer.cancel();
+      tagTimer.purge();
+
+      tagSlowLoopTimer.cancel();
+      tagSlowLoopTimer.purge();
+      timePoster.stop();
+
+      activityTypeTimer.cancel();
+      activityTypeTimer.purge();
+
+      activityTypeSlowLoopTimer.cancel();
+      activityTypeSlowLoopTimer.purge();
+
+      connectorExecutor.get().shutdownNow();
+      if (!connectorExecutor.get().awaitTermination(60, TimeUnit.SECONDS)) {
+        log.error("Failed to gracefully stop connector. Halting process now");
+        System.exit(-1);
+      }
+
+      connectorModule.getApiClient().shutdown();
+
+    } catch (Exception e) {
+      log.warn("There was an error while stopping the connector", e);
+    } finally {
+      connectorExecutor.set(null);
+      wiseTimeConnector.shutdown();
+    }
+  }
+
+  @Override
+  public boolean isHealthy() {
+    return healthRunner.checkBaseLibraryHealth();
+  }
+
+  @Override
+  public MetricInfo getMetrics() {
+    return metricService.getMetrics();
+  }
+
+  @Override
+  public ConnectorModule getConnectorModule() {
+    return connectorModule;
+  }
+
+  private TimePoster createTimePoster(ConnectorControllerConfiguration configuration,
+      ApiClient apiClient,
+      SqLiteHelper sqLiteHelper) {
+    final ConnectorControllerBuilderImpl.PostedTimeLoadMode mode = configuration.getPostedTimeLoadMode();
+    switch (mode) {
+      case LONG_POLL:
+        return new FetchClientTimePoster(
+            wiseTimeConnector,
+            apiClient,
+            healthRunner,
+            connectorExecutor::get,
+            sqLiteHelper,
+            configuration.getFetchClientLimit());
+      case DISABLED:
+        return new NoOpTimePoster();
+      default:
+        log.error("unknown posted time fetch mode={}, defaulting to {}", mode, NoOpTimePoster.class.getName());
+        return new NoOpTimePoster();
+    }
+  }
+
+  private TagRunner createTagRunner(ConnectorControllerConfiguration configuration,
+      WiseTimeConnector wiseTimeConnector) {
+    switch (configuration.getTagScanMode()) {
+      case ENABLED:
+        return new TagRunner(wiseTimeConnector);
+      case DISABLED:
+        return new NoOpTagRunner();
+      default:
+        log.error("Unexpected tag runner mode {}. Fallback to ENABLED", configuration.getTagScanMode());
+        return new TagRunner(wiseTimeConnector);
+    }
+  }
+
+  private TagSlowLoopRunner createTagSlowLoopRunner(ConnectorControllerConfiguration configuration,
+      WiseTimeConnector wiseTimeConnector) {
+    switch (configuration.getTagScanMode()) {
+      case ENABLED:
+        return new TagSlowLoopRunner(wiseTimeConnector);
+      case DISABLED:
+        return new NoOpTagSlowLoopRunner();
+      default:
+        log.error("Unexpected tag runner mode {}. Fallback to ENABLED", configuration.getTagScanMode());
+        return new TagSlowLoopRunner(wiseTimeConnector);
+    }
+  }
+
+  private ActivityTypeRunner createActivityTypeRunner(ConnectorControllerConfiguration configuration,
+      WiseTimeConnector wiseTimeConnector) {
+    switch (configuration.getActivityTypeScanMode()) {
+      case ENABLED:
+        return new ActivityTypeRunner(wiseTimeConnector);
+      case DISABLED:
+        return new NoOpActivityTypeRunner();
+      default:
+        log.error("Unexpected activity type runner mode {}. Fallback to ENABLED", configuration.getTagScanMode());
+        return new ActivityTypeRunner(wiseTimeConnector);
+    }
+  }
+
+  private ActivityTypeSlowLoopRunner createActivityTypeSlowLoopRunner(ConnectorControllerConfiguration configuration,
+      WiseTimeConnector wiseTimeConnector) {
+    switch (configuration.getActivityTypeScanMode()) {
+      case ENABLED:
+        return new ActivityTypeSlowLoopRunner(wiseTimeConnector);
+      case DISABLED:
+        return new NoOpActivityTypeSlowLoopRunner();
+      default:
+        log.error("Unexpected activity type runner mode {}. Fallback to ENABLED", configuration.getTagScanMode());
+        return new ActivityTypeSlowLoopRunner(wiseTimeConnector);
+    }
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class TimerTaskSchedule {
+
+    private long initialDelayMs;
+    private long periodMs;
+  }
+}
---
+++ wisetime-connector/src/test/java/io/wisetime/connector/test_util/FakerUtil.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.test_util;
+
+import com.github.javafaker.Faker;
+
+/**
+ * @author thomas.haines
+ */
+public class FakerUtil {
+
+  private final Faker faker;
+
+  public FakerUtil(Faker faker) {
+    this.faker = faker;
+  }
+
+  public FakerUtil() {
+    this(new Faker());
+  }
+
+  public Faker faker() {
+    return faker;
+  }
+
+  public String getRandom(String prefix) {
+    return faker.numerify(prefix + "#######");
+  }
+
+}
