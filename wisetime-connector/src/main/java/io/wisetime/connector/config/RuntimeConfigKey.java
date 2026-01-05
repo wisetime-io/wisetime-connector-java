a9945a66cf254fa01039569905e504a17f7a687d
Vadym S <vsukolen@anaqua.com>
2026-01-05 07:19:59 +0000

    Mirror Repo

---
+++ wisetime-connector/src/main/java/io/wisetime/connector/config/RuntimeConfigKey.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Practice Insight Pty Ltd. All Rights Reserved.
+ */
+
+package io.wisetime.connector.config;
+
+/**
+ * @author thomas.haines
+ */
+public interface RuntimeConfigKey {
+
+  /**
+   * @return Value used for the environment variable or system property (case insensitive).
+   */
+  String getConfigKey();
+}
